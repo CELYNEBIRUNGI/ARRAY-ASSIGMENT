6b4eab41443b2b82fe961c41c7ebe05636830339
CELYNEBIRUNGI <birungilyn@gmail.com>
2022-10-28 15:30:13 +0300

    array commit

---
+++ array.js
@@ -0,0 +1,32 @@
+
+// AN example  on each of the array
+
+const a = ["celyne", "evelyne", "tuti"];
+for (const element of a) { // You can use `let` instead of `const` if you like
+    console.log(element);
+}
+// a
+// b
+// c
+// the  above we have "a" constant that stores in data for celyne, evelyne tuti.
+// the above is an example of data structure in an array
+// this an example of "for of loop"
+
+// example 2 of AN Array 
+// for each loop
+const b = ["celyne", "evelyne", "birungi"];
+a.forEach((element) => {
+    console.log(element);
+});
+// this case consatant b stores values for celyne, evelyne and birungi
+// Each accepts a callback function and, optionally, a value to use as this when
+// calling that callback (not used above). The callback is called for each
+//element in non-existent elements in sparse arrays
+
+
+
+
+
+
+
+
